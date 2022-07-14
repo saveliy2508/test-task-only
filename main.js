4464578c0aa12661a731ad06e446ef5e2550d952
Saveliy <svolskey2929@gmail.com>
2022-07-14 19:42:09 +0300

    final commit

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const spoilers = document.querySelectorAll('.item_subtitle')
 
 const handleClick = (e) => {
   e.currentTarget.nextElementSibling.classList.toggle('textToggle')
-  console.log(1)
+  e.currentTarget.childNodes[3].classList.toggle('activeButton')
+  e.currentTarget.childNodes[1].classList.toggle('activeSubtitle')
 }
 
 spoilers.forEach((item) => item.addEventListener('click', handleClick))
